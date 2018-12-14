f8d714d252ab8e653a576d6677947895a68aac56
Kinky Cookie <kuki_kukula@yahoo.com>
2018-12-14 15:56:15 -0600

    readme

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE burgers(
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR (255) NOT NULL,
     devoured BOOLEAN DEFAULT FALSE,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
 );
