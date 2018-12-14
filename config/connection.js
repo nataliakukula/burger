fd9aa12e199bc44eb94c691b77c3c9933e2018c8
Kinky Cookie <kuki_kukula@yahoo.com>
2018-12-14 13:41:03 -0600

    database set up for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,20 @@ const mysql = require("mysql");
 require('dotenv').config();
 
 //Connect the server to mySQL database
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 8889,
-    database: "burgers_db",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    socket: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 8889,
+        database: "burgers_db",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        socket: "/Applications/MAMP/tmp/mysql/mysql.sock"
+    });
+}
 
 connection.connect(function (err) {
     if (err) throw err;
